3c8597a60a65e3a3178757b48b6bff21a13682d1
Sean Spurlin <sks0024@auburn.edu>
2015-04-23 19:53:03 -0500

    Refactored StoreLayoutWindow Made that change we talked about with using a set. Also did some general refactoring of all the GUI creation. Made a function which returns a Map of all the locations. Key is the number (like Location 1) and the value is the String (like "Store floor")

--- edu/auburn/eng/rfid_4710/manager_gui/StoreLayoutWindow.java
+++ edu/auburn/eng/rfid_4710/manager_gui/StoreLayoutWindow.java
@@ -1,7 +1,10 @@
 package edu.auburn.eng.rfid_4710.manager_gui;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
@@ -15,14 +18,14 @@ import org.eclipse.swt.widgets.Button;
 public class StoreLayoutWindow {
 
 	protected Shell shlCreateStoreLayout;
-	private Text text;
-	private Text text_1;
-	private Text text_2;
-	private Text text_3;
-	private Text text_4;
-	private Text text_5;
-	private Text text_6;
-	private Text text_7;
+	private Text loc1Text;
+	private Text loc2Text;
+	private Text loc3Text;
+	private Text loc4Text;
+	private Text loc5Text;
+	private Text loc6Text;
+	private Text loc7Text;
+	private Text loc8Text;
 	private ArrayList<String> storeLayout;
 
 	/**
@@ -71,80 +74,19 @@ public class StoreLayoutWindow {
 		shlCreateStoreLayout = new Shell();
 		shlCreateStoreLayout.setSize(558, 322);
 		shlCreateStoreLayout.setText("Create Store Layout");
-		
-		text = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text.setBounds(139, 27, 156, 21);
-		
-		text_1 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_1.setBounds(139, 54, 156, 21);
-		
-		Label lblLocation = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation.setBounds(35, 57, 85, 15);
-		lblLocation.setText("Location #2:");
-		
-		Label lblLocation_1 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_1.setText("Location #1:");
-		lblLocation_1.setBounds(35, 30, 85, 15);
-		
-		Label lblLocation_2 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_2.setText("Location #3:");
-		lblLocation_2.setBounds(35, 84, 85, 15);
-		
-		Label lblLocation_3 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_3.setText("Location #4:");
-		lblLocation_3.setBounds(35, 111, 85, 15);
-		
-		Label lblLocation_4 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_4.setText("Location #5:");
-		lblLocation_4.setBounds(35, 138, 85, 15);
-		
-		Label lblLocation_5 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_5.setText("Location #6:");
-		lblLocation_5.setBounds(35, 165, 85, 15);
-		
-		text_2 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_2.setBounds(139, 81, 156, 21);
-		
-		text_3 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_3.setBounds(139, 108, 156, 21);
-		
-		text_4 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_4.setBounds(139, 135, 156, 21);
-		
-		text_5 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_5.setBounds(139, 162, 156, 21);
-		
-		text_6 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_6.setBounds(139, 189, 156, 21);
-		
-		text_7 = new Text(shlCreateStoreLayout, SWT.BORDER);
-		text_7.setBounds(139, 216, 156, 21);
-		
-		Label lblLocation_6 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_6.setText("Location #7:");
-		lblLocation_6.setBounds(35, 192, 85, 15);
-		
-		Label lblLocation_7 = new Label(shlCreateStoreLayout, SWT.NONE);
-		lblLocation_7.setText("Location #8:");
-		lblLocation_7.setBounds(35, 219, 85, 15);
-		
-		Label lblNewLabel = new Label(shlCreateStoreLayout, SWT.WRAP);
-		lblNewLabel.setBounds(357, 84, 139, 109);
-		lblNewLabel.setText("Please enter names for the locations within your store. You may enter a maximum of 8 locations to be supported by this scanner.");
-		
-		Button btnSaveLayout = new Button(shlCreateStoreLayout, SWT.NONE);
-		btnSaveLayout.setBounds(357, 216, 75, 25);
-		btnSaveLayout.setText("Save Layout");
-		btnSaveLayout.addMouseListener(new MouseListener() {
-			public void mouseDown(MouseEvent e) {
-				storeLayout = getLocationListFromFields();
-				RFIDConfigurationManager.createStoreLocations(storeLayout);
-				shlCreateStoreLayout.dispose();
-			}
-			public void mouseUp(MouseEvent e) { }
-			public void mouseDoubleClick(MouseEvent e) { }
-		});
-		
+		createLocationUI();
+		createLayoutLabel();
+		createSaveButton();
+		createLoadButton();
+	}
+
+	private void createLayoutLabel() {
+		Label layoutLabel = new Label(shlCreateStoreLayout, SWT.WRAP);
+		layoutLabel.setBounds(357, 84, 139, 109);
+		layoutLabel.setText("Please enter names for the locations within your store. You may enter a maximum of 8 locations to be supported by this scanner.");
+	}
+
+	private void createLoadButton() {
 		Button btnCancel = new Button(shlCreateStoreLayout, SWT.NONE);
 		btnCancel.setBounds(442, 216, 75, 25);
 		btnCancel.setText("Cancel");
@@ -157,32 +99,134 @@ public class StoreLayoutWindow {
 		});
 	}
 
-	private ArrayList<String> getLocationListFromFields() {
-		ArrayList<String> locations = new ArrayList<String>();
-		if (!text.getText().trim().equals("") && !locations.contains(text.getText())) {
-			locations.add(text.getText());
+	private void createSaveButton() {
+		Button btnSaveLayout = new Button(shlCreateStoreLayout, SWT.NONE);
+		btnSaveLayout.setBounds(357, 216, 75, 25);
+		btnSaveLayout.setText("Save Layout");
+		btnSaveLayout.addMouseListener(new MouseListener() {
+			public void mouseDown(MouseEvent e) {
+				sendStoreLayoutToConfigManager();
+				shlCreateStoreLayout.dispose();
+			}
+			public void mouseUp(MouseEvent e) { }
+			public void mouseDoubleClick(MouseEvent e) { }
+		});
+	}
+
+	private void createLocationUI() {
+		createLocation1UI();
+		createLocation2UI();
+		createLocation3UI();
+		createLocation4UI();
+		createLocation5UI();
+		createLocation6UI();
+		createLocation7UI();
+		createLocation8UI();
+	}
+
+	private void createLocation8UI() {
+		loc8Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc8Text.setBounds(139, 216, 156, 21);
+		
+		Label lblLocation_8 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_8.setText("Location #8:");
+		lblLocation_8.setBounds(35, 219, 85, 15);
+	}
+
+	private void createLocation7UI() {
+		loc7Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc7Text.setBounds(139, 189, 156, 21);
+		
+		Label lblLocation_7 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_7.setText("Location #7:");
+		lblLocation_7.setBounds(35, 192, 85, 15);
+	}
+
+	private void createLocation6UI() {
+		loc6Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc6Text.setBounds(139, 162, 156, 21);
+		
+		Label lblLocation_6 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_6.setText("Location #6:");
+		lblLocation_6.setBounds(35, 165, 85, 15);
+	}
+
+	private void createLocation5UI() {
+		loc5Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc5Text.setBounds(139, 135, 156, 21);
+		
+		Label lblLocation_5 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_5.setText("Location #5:");
+		lblLocation_5.setBounds(35, 138, 85, 15);
+	}
+
+	private void createLocation4UI() {
+		loc4Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc4Text.setBounds(139, 108, 156, 21);
+		
+		Label lblLocation_4 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_4.setText("Location #4:");
+		lblLocation_4.setBounds(35, 111, 85, 15);
+	}
+
+	private void createLocation3UI() {
+		loc3Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc3Text.setBounds(139, 81, 156, 21);
+		
+		Label lblLocation_3 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_3.setText("Location #3:");
+		lblLocation_3.setBounds(35, 84, 85, 15);
+	}
+
+	private void createLocation2UI() {
+		loc2Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc2Text.setBounds(139, 54, 156, 21);
+		
+		Label lblLocation_2 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_2.setBounds(35, 57, 85, 15);
+		lblLocation_2.setText("Location #2:");
+	}
+
+	private void createLocation1UI() {
+		loc1Text = new Text(shlCreateStoreLayout, SWT.BORDER);
+		loc1Text.setBounds(139, 27, 156, 21);
+		
+		Label lblLocation_1 = new Label(shlCreateStoreLayout, SWT.NONE);
+		lblLocation_1.setText("Location #1:");
+		lblLocation_1.setBounds(35, 30, 85, 15);
+	}
+
+	private ArrayList<String> getStoreLocations() {
+		Set<String> locations = new HashSet<String>();
+		HashMap<Integer, String> locationTextFieldStrings = getLocationsFromFields();
+		for (int i = 0; i < locationTextFieldStrings.size(); i++) {
+			String location = locationTextFieldStrings.get(new Integer(i + 1));
+			if (notNullOrEmpty(location)) {
+				locations.add(location);
+			}
 		}
-		if (!text_1.getText().trim().equals("") && !locations.contains(text_1.getText())) {
-			locations.add(text_1.getText());
-		}
-		if (!text_2.getText().trim().equals("") && !locations.contains(text_2.getText())) {
-			locations.add(text_2.getText());
-		}
-		if (!text_3.getText().trim().equals("") && !locations.contains(text_3.getText())) {
-			locations.add(text_3.getText());
-		}
-		if (!text_4.getText().trim().equals("") && !locations.contains(text_4.getText())) {
-			locations.add(text_4.getText());
-		}
-		if (!text_5.getText().trim().equals("") && !locations.contains(text_5.getText())) {
-			locations.add(text_5.getText());
-		}
-		if (!text_6.getText().trim().equals("") && !locations.contains(text_6.getText())) {
-			locations.add(text_6.getText());
-		}
-		if (!text_7.getText().trim().equals("") && !locations.contains(text_7.getText())) {
-			locations.add(text_7.getText());
-		}
+		return new ArrayList<String>(locations);
+	}
+
+	private boolean notNullOrEmpty(String location) {
+		return location != null && !location.equals("");
+	}
+
+	private void sendStoreLayoutToConfigManager() {
+		storeLayout = getStoreLocations();
+		RFIDConfigurationManager.createStoreLocations(storeLayout);
+	}
+	
+	private HashMap<Integer, String> getLocationsFromFields() {
+		HashMap<Integer, String> locations = new HashMap<Integer, String>();
+		locations.put(1, loc1Text.getText().trim());
+		locations.put(2, loc2Text.getText().trim());
+		locations.put(3, loc3Text.getText().trim());
+		locations.put(4, loc4Text.getText().trim());
+		locations.put(5, loc5Text.getText().trim());
+		locations.put(6, loc6Text.getText().trim());
+		locations.put(7, loc7Text.getText().trim());
+		locations.put(8, loc8Text.getText().trim());
 		return locations;
 	}
 }
